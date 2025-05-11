a3529e60a7c50bcffc398aeb3651d6fdfa8f04bc
Minal Patil <patilminal322@gmail.com>
2025-05-11 19:28:12 +0530

    Optimized code for accountSuccess and homepage

--- pages/account.py
+++ pages/account.py
@@ -1,12 +1,11 @@
 from selenium.webdriver.common.by import By
+from pages.BasePage import BasePage
 
-
-class AccountPage:
+class AccountPage(BasePage):
     def __init__(self,driver):
-        self.driver = driver
+        super().__init__(driver)
 
-    edit_your_account_info_option_linktext = "Edit your account information"
-
+    edit_your_account_info_option_link_text = "Edit your account information"
 
     def display_status_of_edit_your_account_info_option(self):
-        return self.driver.find_element(By.LINK_TEXT,self.edit_your_account_info_option_linktext).is_displayed()
+        return self.element_displayed_status("edit_your_account_info_option_link_text",self.edit_your_account_info_option_link_text)
--- pages/homepage.py
+++ pages/homepage.py
@@ -1,37 +1,35 @@
-from selenium.webdriver.common.by import By
-
 from pages.login import LoginPage
 from pages.register import RegisterPage
 from pages.search import SearchPage
-from selenium import webdriver
-# 3:30:00 session
-class HomePage:
+from pages.BasePage import  BasePage
+
+
+class HomePage(BasePage):
     def __init__(self,driver):
-        self.driver = driver
+        super().__init__(driver)
 
     search_box_field_name = "search"
     search_button_xpath = "//button[contains(@class,'btn-default')]"
     my_account_drop_menu_xpath = "//span[text()='My Account']"
-    login_option_linktext = "Login"
-    register_option_linktext = "Register"
+    login_option_link_text = "Login"
+    register_option_link_text = "Register"
 
     def enter_product_into_search_box_field(self,product_name):
-        self.driver.find_element(By.NAME, self.search_box_field_name).click()
-        self.driver.find_element(By.NAME, self.search_box_field_name).clear()
-        self.driver.find_element(By.NAME,self.search_box_field_name).send_keys(product_name)
+        self.Type(product_name,"search_box_field_name",self.search_box_field_name)
 
     def click_on_search_button(self):
-        self.driver.find_element(By.XPATH,self.search_button_xpath).click()
+        self.element_click("search_button_xpath",self.search_button_xpath)
         return SearchPage(self.driver)
+
     def click_on_my_account_drop_menu(self):
-        self.driver.find_element(By.XPATH, self.my_account_drop_menu_xpath).click()
+        self.element_click("my_account_drop_menu_xpath",self.my_account_drop_menu_xpath)
 
     def click_on_register_drop_menu(self):
-        self.driver.find_element(By.LINK_TEXT, self.register_option_linktext).click()
+        self.element_click("register_option_link_text",self.register_option_link_text)
         return RegisterPage(self.driver)
 
     def select_login_option(self):
-        self.driver.find_element(By.LINK_TEXT,self.login_option_linktext).click()
+        self.element_click("login_option_link_text",self.login_option_link_text)
         return LoginPage(self.driver)
 
     def search_for_a_product(self,product_name):

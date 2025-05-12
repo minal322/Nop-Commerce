6dd89fbe88361cab247021171239636dcad2f178
Minal Patil <patilminal322@gmail.com>
2025-05-12 16:53:22 +0530

    Added fixture to generate snapshot on failed test case using log_on_failure fixture

--- test_cases/BaseTest.py
+++ test_cases/BaseTest.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 import pytest
 
-@pytest.mark.usefixtures("setup_and_teardown")
+@pytest.mark.usefixtures("setup_and_teardown","log_on_failure")
 class BaseTest:
     def generate_random_email(self):
         time_stamp= datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
